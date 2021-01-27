1c774bdfe84247c98a7420a4ab372e5551782059
Azzouz <menaaziz27@gmail.com>
2021-01-27 13:43:27 +0200

    rendering a list of videos

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div>
+        <SearchBar @termChange="onTermChange"></SearchBar>
+        <VideoList></VideoList>
+    </div>
 </template>
-
+    
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
+import SearchBar from './components/SearchBar'
+import VideoList from './components/VideoList';
+const API_KEY = 'AIzaSyDk1v2F8gDJZiFNOQQf2NQN7RwE6Ue0TOI'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    components: {
+        SearchBar,
+        VideoList
+    },
+    methods: {
+        onTermChange: function(searchQuery) {
+            axios.get('https://www.googleapis.com/youtube/v3/search', {
+                params: {
+                    key: API_KEY,
+                    type: 'video',
+                    part: 'snippet',
+                    q: searchQuery
+                }
+            }).then(response => console.log(response)).catch(err => console.log(err))
+        }
+    }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
