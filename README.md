24da4f66f011af391744c14dca7670d02dff92f5
Azzouz <menaaziz27@gmail.com>
2021-01-28 16:57:48 +0200

    edited READme.md

--- README.md
+++ README.md
@@ -1,24 +1,47 @@
 # video-browser
 
+simple mimic youtube application based on vue
+
+### requirements
+
+- replace **API_KEY** in the _App.vue_ with your api key
+
+### how the app looks like
+
+<p>
+    <img src="./assets/one.jpg">
+</p>
+
+and if you clicked on any result
+
+<p>
+    <img src="./assets/one.jpg">
+</p>
+
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Lints and fixes files
+
 ```
 npm run lint
 ```
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
   <div class="container">
     <SearchBar @termChange="onTermChange"></SearchBar>
     <div class="row">
-        <VideoDetail :video="selectedVideo" />
-        <VideoList @videoSelect="onVideoSelect" :videos="videos"></VideoList>
+      <VideoDetail :video="selectedVideo" />
+      <VideoList @videoSelect="onVideoSelect" :videos="videos"></VideoList>
     </div>
   </div>
 </template>
@@ -13,25 +13,25 @@ import axios from "axios";
 import SearchBar from "./components/SearchBar";
 import VideoList from "./components/VideoList";
 import VideoDetail from "./components/VideoDetail";
-const API_KEY = "YOUTUBE_API_KEY";
+const API_KEY = "";
 
 export default {
   name: "App",
   components: {
     SearchBar,
     VideoList,
-    VideoDetail
+    VideoDetail,
   },
   data: function() {
     return {
       videos: [],
-      selectedVideo : null
+      selectedVideo: null,
     };
   },
   methods: {
-      onVideoSelect(video) {
-          this.selectedVideo = video;
-      },
+    onVideoSelect(video) {
+      this.selectedVideo = video;
+    },
     onTermChange: function(searchQuery) {
       axios
         .get("https://www.googleapis.com/youtube/v3/search", {
